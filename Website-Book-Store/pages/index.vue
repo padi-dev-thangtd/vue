d53ebd54fd63d7bb7af4b6ac2de457a280c39000
ThangTr <thangtd@paditech.com>
2022-03-12 00:09:48 +0700

    product homepage

--- Website-Book-Store/pages/index.vue
+++ Website-Book-Store/pages/index.vue
@@ -1,37 +1,36 @@
 <template>
   <div>
     <div class="carousel-introduce d-flex mt-3">
-      <base-carousel :slideToShow="1" :listImages="listImages" class="mr-4"/>
-      <img src="~/assets/images/sale20.svg" alt="" loading="lazy">
+      <base-carousel :slideToShow="1" :listImages="listImages" class="mr-4" />
+      <img src="~/assets/images/sale20.svg" alt="" loading="lazy" />
     </div>
     <div class="book-hot mt-4">
       <div class="book-hot__title">
-        <img src="~/assets/images/giasoc.svg" alt="" loading="lazy">
-        <img src="~/assets/images/flash.gif" alt="" loading="lazy">
-        <img src="~/assets/images/homnay.svg" alt="" loading="lazy">
+        <img src="~/assets/images/giasoc.svg" alt="" loading="lazy" />
+        <img src="~/assets/images/flash.gif" alt="" loading="lazy" />
+        <img src="~/assets/images/homnay.svg" alt="" loading="lazy" />
       </div>
       <div class="p-3">
-        <base-carousel :slideToShow="5" :listStory="dataProduct"/>
+        <base-carousel :slideToShow="5" :listStory="listProducts || []" />
       </div>
     </div>
     <div class="image-introduce mt-4">
-      <img src="~/assets/images/zalopay.png" alt="" loading="lazy">
-      <img src="~/assets/images/sua.jpg" alt="" loading="lazy">
-      <img src="~/assets/images/sua2.jpg" alt="" loading="lazy">
-      <img src="~/assets/images/sua3.jpg" alt="" loading="lazy" class="mr-0">
+      <img src="~/assets/images/zalopay.png" alt="" loading="lazy" />
+      <img src="~/assets/images/sua.jpg" alt="" loading="lazy" />
+      <img src="~/assets/images/sua2.jpg" alt="" loading="lazy" />
+      <img src="~/assets/images/sua3.jpg" alt="" loading="lazy" class="mr-0" />
     </div>
     <div class="book-hot mt-5">
       <div class="p-3">
-        <base-carousel :slideToShow="5" :listStory="dataProduct"/>
+        <base-carousel :slideToShow="5" :listStory="listProducts || []" />
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import baseCarousel from '../components/formItems/baseCarousel.vue';
-import { mapState } from 'vuex';
+import baseCarousel from "../components/formItems/baseCarousel.vue";
+import { mapState } from "vuex";
 export default {
   components: { baseCarousel },
   // middleware: "authenticated",
@@ -41,31 +40,31 @@ export default {
         "https://cdn0.fahasa.com/media/magentothem/banner7/AirPAyT22021_920x420.jpg",
         "https://cdn0.fahasa.com/media/magentothem/banner7/vnpayt2-920x420.jpg",
         "https://cdn0.fahasa.com/media/magentothem/banner7/MOCA-1T01-920x420.png",
-        "https://cdn0.fahasa.com/media/magentothem/banner7/Game_Trang-chung-920x420.jpg",
+        "https://cdn0.fahasa.com/media/magentothem/banner7/Game_Trang-chung-920x420.jpg"
       ],
-      dataProduct: null,
-    }
+      dataProduct: null
+    };
   },
 
   computed: {
     ...mapState({
       listProducts: state => state.products.listProducts,
-      listProductSearch: state => state.products.listProductSearch,
-    }),
+      listProductSearch: state => state.products.listProductSearch
+    })
   },
   watch: {
     listProductSearch(data) {
-      this.dataProduct = data.length > 0 ? data : this.listProducts
+      this.dataProduct = data.length > 0 ? data : this.listProducts;
     }
   },
 
   created() {
-    this.$store.dispatch('products/getListProduct');
+    this.$store.dispatch("products/getListProduct");
     this.dataProduct = this.listProducts;
-  },
-}
+  }
+};
 </script>
 
 <style lang="scss">
-  @import '~/assets/style/page/index';
+@import "~/assets/style/page/index";
 </style>
